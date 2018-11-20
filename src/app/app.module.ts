fdf5a15653dcdec4606f4dec66eb33a6aa11fe5d
robertka <robertka@infosupport.com>
2018-11-20 10:23:00 +0100

    Custom buttons toegevoegd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { WebIntent } from '@ionic-native/web-intent';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -34,6 +36,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    WebIntent,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
